c877183888ba272d604fdf75d732bc5a0acc2c8c
Mark_Vermillion <uglyonenine@yahoo.com>
2021-08-16 18:07:07 -0400

    Updated Station Stats description

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -65,7 +65,7 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """Displays statistics on the most popular stations and trip. Includes most common start, end and start/end combo stations"""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
